63557d1c87941bb5e0a3f5a9bf58e53249187293
HaonanWu <whn289467822@outlook.com>
2017-12-04 17:11:43 -0500

    add user management

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class MyUser(models.Model):
+    user = models.OneToOneField(User)
+    nickname = models.CharField(max_length=16)
+    ucity = models.CharField(max_length=16)
+
+    def __unicode__(self):
+        return self.user.username
